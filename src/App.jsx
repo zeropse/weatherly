da8b15f6430e8adf04e5500a915bd997ac40c1ae
zeropse <129523087+zeropse@users.noreply.github.com>
2026-03-21 01:31:12 +0530

    add lazy load to reduce build size

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { ThemeProvider } from "@/style/theme-provider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import NotFound from "@/pages/NotFound";
+import { lazy, Suspense } from "react";
 import Layout from "@/layout/Layout";
+import { Spinner } from "@/components/ui/spinner";
+
+const Home = lazy(() => import("@/pages/Home"));
+const About = lazy(() => import("@/pages/About"));
+const NotFound = lazy(() => import("@/pages/NotFound"));
 
 export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="flex h-screen w-full items-center justify-center">
+              <Spinner className="size-8 text-primary" />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </ThemeProvider>
   );
